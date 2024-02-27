f78012a0f44ae6883d327d5279b0ec562cafe653
bvssel <rachedwassim.bassel@effyis-partners.ma>
2024-02-27 12:28:15 -0500

    sss

--- sync_script.sh
+++ sync_script.sh
@@ -1,25 +1,18 @@
-#!/bin/bash
+!/bin/bash
 
-# Define repository URLs
 GITLAB_REPO="git@gitlab.com:pfe20243/mirroring.git"
 GITHUB_REPO="git@github.com:bvssel/mirroringRepo.git"
 
-# Define a temporary directory to clone the GitLab repo
 TEMP_DIR=$(mktemp -d)
 
-# Clone the GitLab repo
 git clone $GITLAB_REPO $TEMP_DIR
 
-# Navigate into the cloned repository
 cd $TEMP_DIR
 
-# Pull latest changes
 git pull origin master
 
-# Push changes to GitHub
 git push $GITHUB_REPO master
 
-# Clean up
 rm -rf $TEMP_DIR
 
 echo "Mirror complete."
